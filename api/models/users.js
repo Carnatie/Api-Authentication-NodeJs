4fe2db54447d830116ac9af6742b943cd6b74a34
Kaio Vinicius <kaio.viniciuspv@gmail.com>
2022-02-21 17:35:57 -0300

    Feat(users): Add user validation

--- api/models/users.js
+++ api/models/users.js
@@ -7,10 +7,38 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {}
   }
   Users.init({
-    username: DataTypes.STRING,
-    email: DataTypes.STRING,
+    username: {
+      type: DataTypes.STRING,
+      validate:{
+      userLenght: function (dado) {
+        if (dado.length <= 3) throw new Error('The username field must be longer than 3 characters')
+      },}
+    },
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: true,
+        isEmail: {
+          args: true,
+          msg: `Email does not meet requirements!`
+        },
+        unique: {
+          args: true,
+          msg: 'Email address already in use!'
+        }
+      }
+    },
     hashedPassword: DataTypes.STRING,
-    password: DataTypes.STRING
+    password: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: true,
+        len: {
+          args: [8, 16],
+          msg: "A senha informada deve ter entre 8 e 16 caracteres!"
+        },
+      }
+    }
   }, {
     sequelize: sequelize,
     modelName: 'Users',
