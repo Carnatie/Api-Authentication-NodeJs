12f6b3c498153508044a5ba2dd0dea4dba8f9e1f
Kaio Vinicius <kaio.viniciuspv@gmail.com>
2022-02-22 13:59:55 -0300

    Feat(users): Token generation

--- api/controllers/loginController.js
+++ api/controllers/loginController.js
@@ -1,8 +1,30 @@
 const db = require('../models')
+const jwt = require('jsonwebtoken')
+
 
 class LoginController {
-    static async login(req,res){
-        res.status(200).json({ msg: " yes "})
+    static async login(req, res) {
+        const {
+            email
+        } = req.body
+        const userWithEmail = await db.Users.findOne({
+            where: {
+                email: email
+            }
+        })
+
+        const token = jwt.sign({
+            id: userWithEmail.id,
+            email: userWithEmail.email
+        }, 'c068a6cf-1ae4-40c1-a455-e8a8a2da6fa0', {
+            expiresIn: '2 days'
+        })
+
+        return res.json({
+            auth: true,
+            token: token
+        })
+
     }
 }
 
